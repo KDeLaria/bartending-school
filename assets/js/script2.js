af282daedcc68104b27cb21ddd7f7ed93024d908
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-01-10 15:00:42 -0600

    script-two

---
+++ assets/js/script2.js
@@ -0,0 +1,7 @@
+//Do this as seperate page js or integrate into original
+
+//listener for clicks on either Make a Recipe Card or Give Up or 
+
+//pull in drink ID
+//re-run api  We need to do this because it's being directed here three different ways:
+//recall
